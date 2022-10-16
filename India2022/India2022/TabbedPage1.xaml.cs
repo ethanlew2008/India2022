1ee635a72c431372739aa327c9a05496d5988819
ethanlew2008 <ethanjlewis2008@gmail.com>
2022-10-16 20:03:12 +0100

    Unified Update Button Code Much More Efficient Update Button Now An Icon Currency Update Bug Fixes

--- India2022/India2022/TabbedPage1.xaml.cs
+++ India2022/India2022/TabbedPage1.xaml.cs
@@ -18,6 +18,8 @@ namespace India2022
     public partial class TabbedPage1 : TabbedPage
     {
         bool flight = false;
+        bool onlyhome = false; 
+
         static bool flash = true;
         Stopwatch flighttime = new Stopwatch();
         Stopwatch sleep = new Stopwatch(); double sleephours = 0;
@@ -25,164 +27,42 @@ namespace India2022
         double countries = 14;
         string input = "";
 
-
-
-
         public TabbedPage1()
         {
             InitializeComponent();
             CurrentPage = Children[1];
-            
 
-            var indiatime = DateTime.UtcNow.AddMinutes(330);
-            indiatime = Convert.ToDateTime(indiatime.ToString("HH:mm"));
-            string temp1 = Convert.ToString(indiatime);
-            temp1 = temp1.Substring(10);
-            temp1 = temp1.Remove(temp1.Length - 3, 3);
-
-            int uktime = DateTime.UtcNow.Hour + 1;
-            string mins = Convert.ToString(DateTime.UtcNow.Minute);
-            if (mins.Length == 1) { mins = "0" + mins; }
-            if (uktime >= 24) { uktime -= 24; }
-
-            var profiles = Connectivity.ConnectionProfiles;
-            if ((CrossConnectivity.Current.IsConnected) && !profiles.Contains(ConnectionProfile.WiFi)) { Connection.Text = "Data On"; }
-            else { Connection.Text = "Data Off"; }
-
-            double battcharge = Battery.ChargeLevel;
-            battcharge *= 100;
-
-            string chargin = "";
-            chargin = Battery.PowerSource.ToString();
-            if (chargin == "AC") { chargin = "Wall Plug"; }
-            else if (chargin == "Usb") { chargin = "USB"; }
-
-            string energysave = "";
-            if (Battery.EnergySaverStatus == EnergySaverStatus.On) { energysave = "Battery Saver On"; }
-            else { energysave = "Battery Saver Off"; }          
-
-            HomeLocalTime.Text = "LOC: " + DateTime.Now.ToString("HH:mm");
-            TimeHomeUK.Text = "LON: " + uktime + ":" + mins;
-            TimeHomeIN.Text = "DEL:" + temp1;
-            Charging.Text = chargin;
-            BatterySaver.Text = energysave;
-            Appversion.Text = AppInfo.VersionString;
-            BatteryLevel.Text = "Batt: " + battcharge + "%";
+            onlyhome = true;
+            UniversialUpdate();
         }
 
         private void StartFlyButton_Clicked(object sender, EventArgs e)
         {
-            if (!flight) { flighttime.Start(); flight = true; UpdateFlyButton.Opacity = 1; StartFlyButton.Text = "Stop"; }
+            if (!flight) { flighttime.Start(); flight = true; UpdateFlyButton.Opacity = 1; StartFlyButton.Text = "Stop"; UniversialUpdate(); }
             else { flight = false; flighttime.Reset(); UpdateFlyButton.Opacity = 0; StartFlyButton.Text = "Start"; }
         }
 
         private void UpdateFlyButton_Clicked(object sender, EventArgs e)
-        {;
-            Time.Text = "";
-            int temp = Convert.ToInt32(3.15e+7 - flighttime.ElapsedMilliseconds); temp /= 1000; temp /= 60;
-            if (temp <= 0) { Time.Text = "Welcome"; flighttime.Reset(); return; }
-            TimeSpan spWorkMin = TimeSpan.FromMinutes(temp);
-            string workHours = spWorkMin.ToString(@"hh\:mm");
-
-            double percentage = Convert.ToDouble(flighttime.ElapsedMilliseconds) / 3.15e+7; percentage *= 100; percentage = 100 - percentage;
-            percentage = Convert.ToInt32(percentage);
-
-            co2 = flighttime.ElapsedMilliseconds; co2 /= 1000; co2 /= 60; co2 *= 4;
-
-            countries = flighttime.ElapsedMilliseconds / 1000; countries /= 60;
-            countries /= 38;
-            countries = Convert.ToInt32(countries);
-
-            if (countries >= 10)
-            {
-                countries = countries - countries;
-                countries *= -1;
-            }
-
-            if (CrossConnectivity.Current.IsConnected) { Airmode.Text = "AirMode: Yes"; }
-            else { Connection.Text = "AirMode: No"; }
-
-            
-
-            var indiatime = DateTime.UtcNow.AddMinutes(330);
-            indiatime = Convert.ToDateTime(indiatime.ToString("HH:mm"));
-            string temp1 = Convert.ToString(indiatime);
-            temp1 = temp1.Substring(10);
-            temp1 = temp1.Remove(temp1.Length - 3, 3);
-
-            int uktime = DateTime.UtcNow.Hour + 1;
-            string mins = Convert.ToString(DateTime.UtcNow.Minute);
-            if (mins.Length == 1) { mins = "0" + mins; }
-            if (uktime >= 24) { uktime -= 24; }
-
-            Time.Text += workHours;
-            Percentage.Text = percentage + "% Left";
-            CountriesFlew.Text = countries + " Countries";
-            LocalTime.Text = DateTime.Now.ToString("HH:mm") + " Local";
-            TimeIn.Text = "IN:" + temp1;
-            TimeUK.Text = "UK: " + uktime + ":" + mins;
-            Alt();
-            Speed();
-
-            if (co2 >= 100) { co2 /= 1000; co2 = Math.Round(co2, 2); CO2.Text = co2 + "T CO2"; }
-            else { co2 = Math.Ceiling(co2); CO2.Text = co2 + "KG CO2"; }
-
-
+        {
+            UniversialUpdate();
         }
 
         private void UpdateButtonHome_Clicked(object sender, EventArgs e)
         {
-            var indiatime = DateTime.UtcNow.AddMinutes(330);
-            indiatime = Convert.ToDateTime(indiatime.ToString("HH:mm"));
-            string temp1 = Convert.ToString(indiatime);
-            temp1 = temp1.Substring(10);
-            temp1 = temp1.Remove(temp1.Length - 3, 3);
-
-            int uktime = DateTime.UtcNow.Hour + 1;
-            string mins = Convert.ToString(DateTime.UtcNow.Minute);
-            if (mins.Length == 1) { mins = "0" + mins; }
-
-
-            var profiles = Connectivity.ConnectionProfiles;
-
-            if ((CrossConnectivity.Current.IsConnected) && !profiles.Contains(ConnectionProfile.WiFi)) { Connection.Text = "Data On"; }
-            else { Connection.Text = "Data Off"; }
-
-            string chargin = "";
-
-            chargin = Battery.PowerSource.ToString();
-            if (chargin == "AC") { chargin = "Wall Plug"; }
-            else if (chargin == "Usb") { chargin = "USB"; }
-
-            if (uktime >= 24) { uktime -= 24; }
-
-            double battcharge = Battery.ChargeLevel;
-            battcharge *= 100;
-
-            string energysave = "";
-            if (Battery.EnergySaverStatus == EnergySaverStatus.On) { energysave = "Battery Saver On"; }
-            else { energysave = "Battery Saver Off"; }
-
-            HomeLocalTime.Text = "LOC: " + DateTime.Now.ToString("HH:mm");
-            TimeHomeUK.Text = "LON: " + uktime + ":" + mins;
-            TimeHomeIN.Text = "DEL:" + temp1;
-            BatteryLevel.Text = "Batt: " + battcharge + "%";
-            BatterySaver.Text = energysave;
-            Appversion.Text = AppInfo.VersionString;
-            Charging.Text = chargin;
+            UniversialUpdate();
         }
 
         private void FlashButton_Clicked(object sender, EventArgs e)
         {
             try
             {
-                double conversion = Convert.ToDouble(CurrencyGBP.Text) / 82.35;
+                double conversion = Convert.ToDouble(CurrencyGBP.Text) / 82.42;
                 int temp = Convert.ToInt32(conversion);
                 conversion = Math.Round(conversion, 2);
                 CurrencyUSD.Text = "$" + conversion;
 
 
-                conversion = Convert.ToDouble(CurrencyGBP.Text) / 92.01;
+                conversion = Convert.ToDouble(CurrencyGBP.Text) / 92.07;
                 temp = Convert.ToInt32(conversion);
                 conversion = Math.Round(conversion, 2);
                 CurrencyGBP.Text = "£" + conversion;
@@ -236,53 +116,7 @@ namespace India2022
 
         private void Updatesleep_Clicked(object sender, EventArgs e)
         {
-
-            if (sleep.IsRunning)
-            {
-                sleephours = sleep.ElapsedMilliseconds / 1000; sleephours /= 60;
-
-                TimeSpan spWorkMin = TimeSpan.FromMinutes(sleephours);
-                string workHours = spWorkMin.ToString(@"hh\:mm");
-
-                var indiatime = DateTime.UtcNow.AddMinutes(330);
-                indiatime = Convert.ToDateTime(indiatime.ToString("HH:mm"));
-                string temp1 = Convert.ToString(indiatime);
-                temp1 = temp1.Substring(10);
-                temp1 = temp1.Remove(temp1.Length - 3, 3);
-
-                int uktime = DateTime.UtcNow.Hour + 1;
-                string mins = Convert.ToString(DateTime.UtcNow.Minute);
-                if (mins.Length == 1) { mins = "0" + mins; }
-
-                double elapsedtime = sleep.ElapsedMilliseconds; elapsedtime /= 1000; elapsedtime /= 60;
-                double percentage = elapsedtime /= 480;
-                percentage = Math.Ceiling(percentage);
-
-
-                double battcharge = Battery.ChargeLevel;
-                battcharge *= 100;
-
-                string energysave = "";
-                if (Battery.EnergySaverStatus == EnergySaverStatus.On) { energysave = "Battery Saver On"; }
-                else { energysave = "Battery Saver Off"; }
-
-
-                SleepPercentOfTarget.Text = percentage + "%";
-                SleepTime.Text = workHours;
-                SleepBreaths.Text = Convert.ToInt32(sleephours * 16) + " Breaths";
-                BattPersleep.Text = "Batt: " + battcharge + "%";
-
-                GBRLocal.Text = "LON: " + uktime + ":" + mins;
-                IndiaLocal.Text = "DEL:" + temp1;     
-                
-                BatterySaver1.Text = energysave;
-                Appversion1.Text = AppInfo.VersionString;
-
-            }
-            else
-            {
-                return;
-            }
+            UniversialUpdate();         
         }
 
         private void Startsleep_Clicked(object sender, EventArgs e)
@@ -296,18 +130,129 @@ namespace India2022
                 FlightPage.IconImageSource = "IndiaPlaneIcon.jpg";
                 IndiaHome.IconImageSource = "IndiaHouseIcon.jpg";
                 IndiaSleep.IconImageSource = "IndiaSleepIcon.jpg";
-
             }
             else
             {
-                sleep.Start();
+                sleep.Start();               
                 Startsleep.Text = "End";
                 Updatesleep.Opacity = 1;
                 BarBackgroundColor = Color.MediumPurple;
                 FlightPage.IconImageSource = "IndiaPlaneIconNight.jpg";
                 IndiaHome.IconImageSource = "IndiaHouseIconNight.jpg";
                 IndiaSleep.IconImageSource = "IndiaSleepIconNight.jpg";
-            }    
+                UniversialUpdate();
+            }
+        }
+
+
+
+
+        public void UniversialUpdate()
+        {
+            //Home
+            var indiatime = DateTime.UtcNow.AddMinutes(330).ToString("HH:mm");
+            var uktime = DateTime.UtcNow.AddMinutes(60).ToString("HH:mm");
+
+            var profiles = Connectivity.ConnectionProfiles;
+
+            if ((CrossConnectivity.Current.IsConnected) && !profiles.Contains(ConnectionProfile.WiFi)) { Connection.Text = "Data On"; }
+            else { Connection.Text = "Data Off"; }
+
+            string chargin = "";
+
+            chargin = Battery.PowerSource.ToString();
+            if (chargin == "AC") { chargin = "Wall Plug"; }
+            else if (chargin == "Usb") { chargin = "USB"; }
+
+
+            double battcharge = Battery.ChargeLevel;
+            battcharge *= 100;
+
+            string energysave = "";
+            if (Battery.EnergySaverStatus == EnergySaverStatus.On) { energysave = "Battery Saver On"; }
+            else { energysave = "Battery Saver Off"; }
+
+            HomeLocalTime.Text = "LOC: " + DateTime.Now.ToString("HH:mm");
+            TimeHomeUK.Text = "LON: " + uktime;
+            TimeHomeIN.Text = "DEL:" + indiatime;
+                
+            BatteryLevel.Text = "Batt: " + battcharge + "%";
+            BatterySaver.Text = energysave;
+            Appversion.Text = AppInfo.VersionString;
+            Charging.Text = chargin;
+
+            //Sleep
+            if (sleep.IsRunning)
+            {
+                sleephours = sleep.ElapsedMilliseconds / 1000; sleephours /= 60;
+
+                TimeSpan spWorkMin2 = TimeSpan.FromMinutes(sleephours);
+                string workHours2 = spWorkMin2.ToString(@"hh\:mm");
+
+                var indiatime2 = DateTime.UtcNow.AddMinutes(330).ToString("HH:mm");
+                var uktime2 = DateTime.UtcNow.AddMinutes(60).ToString("HH:mm");
+
+                double percentage2 = sleep.ElapsedMilliseconds / 2.88e+7;
+
+                double battcharge2 = Battery.ChargeLevel;
+                battcharge *= 100;
+
+                string energysave2 = "";
+                if (Battery.EnergySaverStatus == EnergySaverStatus.On) { energysave = "Battery Saver On"; }
+                else { energysave = "Battery Saver Off"; }
+
+                SleepPercentOfTarget.Text = Convert.ToInt32((1 - percentage2) * 100) + "%";
+                SleepTime.Text = workHours2;
+                SleepBreaths.Text = Convert.ToInt32(sleephours * 16) + " Breaths";
+                BattPersleep.Text = "Batt: " + battcharge2 + "%";
+
+                GBRLocal.Text = "LON: " + uktime2;
+                IndiaLocal.Text = "DEL:" + indiatime2;
+
+                BatterySaver1.Text = energysave;
+                Appversion1.Text = AppInfo.VersionString;
+            }
+
+            //Planes
+            if (flight)
+            {
+                Time.Text = "";
+                int temp = Convert.ToInt32(3.15e+7 - flighttime.ElapsedMilliseconds);
+                if (temp <= 0) { Time.Text = "Welcome"; flighttime.Reset(); return; }
+                TimeSpan spWorkMin = TimeSpan.FromMilliseconds(temp);
+                string workHours = spWorkMin.ToString(@"hh\:mm");
+
+                double percentage = flighttime.ElapsedMilliseconds / 3.15e+7;
+
+                co2 = flighttime.ElapsedMilliseconds * 0.0015;
+
+                countries = flighttime.ElapsedMilliseconds / 1000; countries /= 60;
+                countries /= 38;
+                countries = Convert.ToInt32(countries);
+                if (countries >= 10) { countries = countries - countries; countries *= -1; }
+
+
+                if (Connectivity.NetworkAccess == NetworkAccess.None) { Airmode.Text = "AirMode: Yes"; }
+                else { Airmode.Text = "AirMode: No"; }
+
+                var indiatime3 = DateTime.UtcNow.AddMinutes(330).ToString("HH:mm");
+                var uktime3 = DateTime.UtcNow.AddMinutes(60).ToString("HH:mm");
+
+                Time.Text += workHours;
+
+                Percentage.Text = Convert.ToInt32((1 - percentage) * 100) + "% Left";
+
+                CO2.Text = Math.Round(co2 / 1000, 2) + "T CO2";
+
+                CountriesFlew.Text = countries + " Countries";
+
+                LocalTime.Text = DateTime.Now.ToString("HH:mm") + " Local";
+                TimeIn.Text = "IN:" + indiatime3;
+                TimeUK.Text = "UK: " + uktime3;
+
+                Alt();
+                Speed();
+            }
         }
     }
 }
